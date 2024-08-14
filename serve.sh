69d6648ae6ef26f5ef57e33466cb68a477d8a163
gtko <gtko@users.noreply.github.com>
2024-08-14 16:41:30 +0200

    config sqlite

--- serve.sh
+++ serve.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+mkdir /home/ubuntu/expose_database
+touch /home/ubuntu/expose_database/expose.db
+
 ./caddy/caddy run --config /var/www/expose/build-config/Caddyfile &
 php expose serve dotshare.dev --config=/var/www/expose/build-config/expose.php
